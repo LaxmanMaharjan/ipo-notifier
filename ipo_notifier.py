23ecfc0511b9fc377c86e6623fb4037000924aaf
Laxman Maharjan <lxmnmrzn@gmail.com>
2023-03-08 15:40:33 +0545

    refactor the code, breakdown into multiple functions

--- ipo_notifier.py
+++ ipo_notifier.py
@@ -1,22 +1,16 @@
 import scrapy
-import pandas as pd
 import datetime
-import smtplib
-import os 
+
+from typing import Tuple
 
 from scrapy.crawler import CrawlerProcess
-from email.message import EmailMessage
 
-#data = pd.read_csv("IPO_Infos.csv")
-receivers = pd.read_csv("Receivers.csv")
-
-current_date = str(datetime.date.today())
-
-receivers_list = receivers.iloc[:, 2].tolist()
+from utilities import EmailService
 
 class IpoNotifierSpider(scrapy.Spider):
     name = 'Ipo_Notifier'
     base_url = "https://www.sharesansar.com/"
+    current_date = str(datetime.date.today())
 
     # custom headers
     headers = {
@@ -30,75 +24,46 @@ class IpoNotifierSpider(scrapy.Spider):
             headers = self.headers,
             callback = self.parse
         )
+    
+    def get_ipo_data(self, response):
+        return {
+        'Company' : response.xpath(".//td/a/@title").get(),
+        'Symbol' : response.xpath(".//td/a/text()").get(),
+        'Units' : response.xpath(".//td[@class= 'text-center'][1]/text()").get(),
+        'Unit_Price' : response.xpath(".//td[@class= 'text-center'][2]/text()").get(),
+        'Opening_Date' : response.xpath(".//td[6]/text()").get().strip(),
+        'Closing_Date' : response.xpath(".//td[7]/text()").get().strip()
+        }
 
+    
+    def create_ipo_message(self, info:str) -> Tuple[dict,str]:
+        ipo_data = self.get_ipo_data(info)
+        ipo_str = '\n'.join([f'{key}: {value}' for key, value in ipo_data.items()])
+        return ipo_data,ipo_str
+
+    def send_mail_on_opening_date(self, info:str, opening_date: str):
+
+        if (opening_date == str(datetime.date.today() + datetime.timedelta(days = 1))) or (current_date == opening_date):
+            ipo_data,ipo_str = self.create_ipo_message(info=info)
+            yield ipo_data
+            email_service = EmailService(ipo_str)
+            email_service.send_mail_to_users()
+        
     def parse(self, response):
         """
-        This function scrapes all the required information about the IPOs
+        This function scrapes allprocess.start() the required information about the IPOs
         """
-        data = pd.read_csv("IPO_Infos.csv")
-        
         for i in range(1,6):
-            for info in response.xpath(f"//div[@id='myTableEip']/table/tbody/tr[{i}]"):
+            for info in response.xpath(
+                f"//div[@id='myTableEip']/table/tbody/tr[{i}]"
+            ):
+                opening_date = info.xpath(".//td[6]/text()").get().strip()
 
-                Company = info.xpath(".//td/a/@title").get()
-                Symbol = info.xpath(".//td/a/text()").get()
-                Units = info.xpath(".//td[@class= 'text-center'][1]/text()").get()
-                Unit_Price = info.xpath(".//td[@class= 'text-center'][2]/text()").get()
-                Opening_Date = info.xpath(".//td[6]/text()").get().strip()
-                Closing_Date = info.xpath(".//td[7]/text()").get().strip()
+                yield from self.send_mail_on_opening_date(
+                    info=info, opening_date=opening_date
+                )
 
-
-                # Notify on day before ipo openinig date and on the opening date
-                
-
-                if (Opening_Date == str(datetime.date.today() + datetime.timedelta(days = 1))) or (current_date == Opening_Date):
-                    
-                    ipo_data = {
-                    'Company' : info.xpath(".//td/a/@title").get(),
-                    'Symbol' : info.xpath(".//td/a/text()").get(),
-                    'Units' : info.xpath(".//td[@class= 'text-center'][1]/text()").get(),
-                    'Unit_Price' : info.xpath(".//td[@class= 'text-center'][2]/text()").get(),
-                    'Opening_Date' : info.xpath(".//td[6]/text()").get().strip(),
-                    'Closing_Date' : info.xpath(".//td[7]/text()").get().strip()
-                    }
-
-
-                    ipo_str = '\n'.join([f'{key}: {value}' for key, value in ipo_data.items()])
-
-                    message = f"This Email is to Notify you to apply in the new IPO.\n\nIPO Details:\n{ipo_str}\n\nYours Always,\nLaxman Maharjan"
-
-                    data1 = data.append(ipo_data,ignore_index=True).sort_values(by = "Opening_Date", ascending = False)
-
-                    yield ipo_data
-
-                    # get email and password from environment variables
-                    EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
-                    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-
-                    msg = EmailMessage()
-                    msg['Subject'] = "New IPO Information"
-                    msg['From'] = EMAIL_ADDRESS
-                    msg['To'] = ', '.join(receivers_list)
-
-                    msg.set_content(message)
-
-
-                    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-                        smtp.send_message(msg)
-                        
-                    #appending new IPO in existing csv file
-#                     if Company in data.Company.tolist() == True:
-#                         continue
-#                     else:
-#                         data1.to_csv("IPO_Infos.csv",index=False)
-                    
-                    if Company in data.Company.tolist() == False:
-                        data1.to_csv("IPO_Infos.csv",index=False)
-
-                
 if __name__=='__main__':
-    #run spider
     process = CrawlerProcess()
     process.crawl(IpoNotifierSpider)
     process.start()
